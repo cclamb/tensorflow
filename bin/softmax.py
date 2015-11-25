de41a0777c2db39f9a60c7891174ca5ffb14fbf3
cclamb <cclamb@sandia.gov>
2015-11-24 18:21:49 -0700

    Beginning convolutional network

--- bin/softmax.py
+++ bin/softmax.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
 import sys
+import pdb
 
 sys.path.append('lib')
 
 import input_data
 
 NUM_CORES = 6
+LEARNING_RATE = 0.01
+MOMENTUM = 0.01
+
+optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE)
+# optimizer = tf.train.AdagradOptimizer(LEARNING_RATE)
+# optimizer = tf.train.MomentumOptimizer(LEARNING_RATE, MOMENTUM)
 
 config = tf.ConfigProto(
     inter_op_parallelism_threads=NUM_CORES,
@@ -23,7 +30,7 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder('float', [None, 10])
 
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+train_step = optimizer.minimize(cross_entropy)
 
 init = tf.initialize_all_variables()
 
@@ -32,9 +39,11 @@ with tf.Session(config=config) as sess:
     for i in xrange(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    correct_prediction = tf.equal(tf.arg_max(y, 1), tf.argmax(y_, 1))
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
     print sess.run(
         accuracy,
         feed_dict={x: mnist.test.images, y_: mnist.test.labels}
     )
+
+print 'Finished.'
